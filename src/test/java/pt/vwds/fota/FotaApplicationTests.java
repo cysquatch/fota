88f6084fb63e61005b7c9649d3defadc46a839c0
goncalo_ribeiro <goncalo.ribeiro@indigita.ch>
2020-07-04 22:59:55 +0100

    init

---
+++ src/test/java/pt/vwds/fota/FotaApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.vwds.fota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
